c6869c563488f1fc0945ca24b3d671ef7750444a
zaithoon03 <153828395+zaithoon03@users.noreply.github.com>
2023-12-14 18:24:33 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,19 @@
+package week1;
+
+public class Student { 
+
+	String studentName="shazana";
+		int rollNo= 55;	
+		String collegeName="Ethiraj";
+		int markScored= 900;
+		float cgpa= 33.2f;
+			
+			public static void main(String[] args) {
+			Student report = new Student();
+			System.out.println(report.studentName);
+			System.out.println(report.rollNo); 
+			System.out.println(report.collegeName);
+			System.out.println(report.markScored);
+			System.out.println(report.cgpa);
+}
+}
